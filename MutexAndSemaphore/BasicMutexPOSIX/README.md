17f86752c3719b09a2d3b87defb743d8630a3c36
H Enes Simsek <enes1406@gmail.com>
2022-09-01 18:04:05 +0300

    bug fix

--- MutexAndSemaphore/BasicMutexPOSIX/README.md
+++ MutexAndSemaphore/BasicMutexPOSIX/README.md
@@ -1,15 +1,69 @@
 # Basic Mutex Usage With Pthread
 Two threads access the same shared source simultaneously. One thread tries to write and the other tries to read.
 
-Firstly the program runs the threads without a sync mechanism, then runs with mutex.   
+Firstly the program runs the threads without a sync mechanism, then runs with mutex. 
+
+# What Do This Program Do?   
+
+Shared array is initialized with zeros. Producer thread writes ones to shared array starting from the beginning. Consumer thread reads the data from the end and writes to another array(but not shared) to see results.
+
 
 # Possible Outcomes
 ```
-sudo gcc main.c -pthread
+gcc main.c -pthread
 ./a.out
-Consumer(without mutex):
-0 0 0 0 0 1 1 1 1 1
+without mutex:
+shared data initialized: [0 0 0 0 0 0 0 0 0 0 ]
+_write[0], data[1 0 0 0 0 0 0 0 0 0 ]
+data[9]=0, data[1 0 0 0 0 0 0 0 0 0 ]
+data[8]=0, data[1 0 0 0 0 0 0 0 0 0 ]
+_write[1], data[1 1 0 0 0 0 0 0 0 0 ]
+data[7]=0, data[1 1 0 0 0 0 0 0 0 0 ]
+_write[2], data[1 1 1 0 0 0 0 0 0 0 ]
+data[6]=0, data[1 1 1 0 0 0 0 0 0 0 ]
+_write[3], data[1 1 1 1 0 0 0 0 0 0 ]
+data[5]=0, data[1 1 1 1 0 0 0 0 0 0 ]
+_write[4], data[1 1 1 1 1 0 0 0 0 0 ]
+data[4]=1, data[1 1 1 1 1 0 0 0 0 0 ]
+_write[5], data[1 1 1 1 1 1 0 0 0 0 ]
+data[3]=1, data[1 1 1 1 1 1 0 0 0 0 ]
+_write[6], data[1 1 1 1 1 1 1 0 0 0 ]
+data[2]=1, data[1 1 1 1 1 1 1 0 0 0 ]
+_write[7], data[1 1 1 1 1 1 1 1 0 0 ]
+data[1]=1, data[1 1 1 1 1 1 1 1 0 0 ]
+_write[8], data[1 1 1 1 1 1 1 1 1 0 ]
+data[0]=1, data[1 1 1 1 1 1 1 1 1 0 ]
+_write[9], data[1 1 1 1 1 1 1 1 1 1 ]
 
-Consumer(with mutex):
-1 1 1 1 1 1 1 1 1 1
+
+What consumer reads: [1 1 1 1 1 0 0 0 0 0 ]
+
+
+with mutex:
+shared data initialized: [0 0 0 0 0 0 0 0 0 0 ]
+_write[0], data[1 0 0 0 0 0 0 0 0 0 ]
+_write[1], data[1 1 0 0 0 0 0 0 0 0 ]
+_write[2], data[1 1 1 0 0 0 0 0 0 0 ]
+_write[3], data[1 1 1 1 0 0 0 0 0 0 ]
+_write[4], data[1 1 1 1 1 0 0 0 0 0 ]
+_write[5], data[1 1 1 1 1 1 0 0 0 0 ]
+_write[6], data[1 1 1 1 1 1 1 0 0 0 ]
+_write[7], data[1 1 1 1 1 1 1 1 0 0 ]
+_write[8], data[1 1 1 1 1 1 1 1 1 0 ]
+_write[9], data[1 1 1 1 1 1 1 1 1 1 ]
+data[9]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+data[8]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+data[7]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+data[6]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+data[5]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+data[4]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+data[3]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+data[2]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+data[1]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+data[0]=1, data[1 1 1 1 1 1 1 1 1 1 ]
+
+
+What consumer reads: [1 1 1 1 1 1 1 1 1 1 ]
 ```
+
+As you can see, without mutex consumer reads wrong data that includes zeros. However, with mutex producer runs first and consumer reads expected data.
--- MutexAndSemaphore/BasicMutexPOSIX/main.c
+++ MutexAndSemaphore/BasicMutexPOSIX/main.c
@@ -3,10 +3,24 @@
 #include <unistd.h>
 
 #define SHARED_DATA_LEN 10
-int shared_data[SHARED_DATA_LEN];
+int shared_data[SHARED_DATA_LEN]; // producer writes, consumer reads
+int consumers_final_data[SHARED_DATA_LEN]; // only consumer writes
 
 pthread_mutex_t mutex;
 
+
+void (int data[])
+{
+    printf("[");
+    int i;
+    for(i = 0; i < SHARED_DATA_LEN; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("]\n");
+}
+
+// init array with zeros
 void init_shared_data()
 {
     int i;
@@ -14,30 +28,38 @@ void init_shared_data()
     {
         shared_data[i] = 0;
     }
+    printf("shared data initialized: ");
+    print_data(shared_data);
 }
 
+// producer writes the shared data
 void* produce(void* arg)
 {
     int i;
     for(i = 0; i < SHARED_DATA_LEN; i++)
     {
         shared_data[i] = 1;
+        printf("_write[%d], data", i);
+        print_data(shared_data);
         usleep(100); // 0.1 ms
     }
 }
 
+// consumer reads the shared data and writes to consumers_final_data
 void* consume(void* arg)
 {
     int i;
-    printf("\nConsumer(without mutex):\n");
     for(i = SHARED_DATA_LEN - 1; i >= 0; i--)
     {
-        printf("%d ",shared_data[i]);
+        consumers_final_data[i] = shared_data[i];
+        printf("data[%d]=%d, data", i, shared_data[i]);
+        print_data(shared_data);
         usleep(100); // 0.1 ms
     }
     printf("\n");
 }
 
+// producer writes the shared data
 void* produce_with_mutex(void* arg)
 {
     pthread_mutex_lock(&mutex);
@@ -46,21 +68,25 @@ void* produce_with_mutex(void* arg)
     for(i = 0; i < SHARED_DATA_LEN; i++)
     {
         shared_data[i] = 1;
+        printf("_write[%d], data", i);
+        print_data(shared_data);
         usleep(100); // 0.1 ms
     }
     
     pthread_mutex_unlock(&mutex);
 }
 
+// consumer reads the shared data and writes to consumers_final_data
 void* consume_with_mutex(void* arg)
 {
     pthread_mutex_lock(&mutex);
   
     int i;
-    printf("\nConsumer(with mutex):\n");
     for(i = SHARED_DATA_LEN - 1; i >= 0; i--)
-    {
-        printf("%d ",shared_data[i]);
+    { 
+        consumers_final_data[i] = shared_data[i];
+        printf("data[%d]=%d, data", i, shared_data[i]);
+        print_data(shared_data);
         usleep(100); // 0.1 ms
     }
     printf("\n");
@@ -72,6 +98,7 @@ void* consume_with_mutex(void* arg)
 int main(void)
 {
     // threads without mutex
+    printf("without mutex:\n");
     init_shared_data();
     pthread_t producer;
     pthread_t consumer;
@@ -82,7 +109,11 @@ int main(void)
     pthread_join(producer, NULL);
     pthread_join(consumer, NULL);
     
+    printf("\nWhat consumer reads: ");
+    print_data(consumers_final_data);
+    
     // threads with mutex
+    printf("\n\nwith mutex:\n");
     init_shared_data();
     pthread_t producer_m;
     pthread_t consumer_m;
@@ -94,6 +125,9 @@ int main(void)
 
     pthread_join(producer_m, NULL);
     pthread_join(consumer_m, NULL);
+    
+    printf("\nWhat consumer reads: ");
+    print_data(consumers_final_data);
     
     pthread_mutex_destroy(&mutex); 
     return 0;
